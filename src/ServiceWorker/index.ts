5a571b41de1f059ab4177e325ccaa7137d22e48c
Kostiantyn Levytskyi <le.konstantos@gmail.com>
2021-06-04 09:18:34 +0300

    feat: added messageSW

--- src/ServiceWorker/index.ts
+++ src/ServiceWorker/index.ts
@@ -6,3 +6,4 @@ export { default as ChangeLogPlugin } from './ChangeLogPlugin';
 export { default as apiResourcesCache } from './apiResourcesCache';
 export { default as forcedInstallation } from './forcedInstallation';
 export { default as imagesCache } from './imagesCache';
+export { default as messageSW } from './messageSW';
